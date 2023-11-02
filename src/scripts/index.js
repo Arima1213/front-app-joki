20cec4825be69177514345c6353fc682c94972e3
Arima1213 <arima3992@gmail.com>
2023-11-03 06:50:47 +0700

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,146 @@
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.css";
+
+console.log("Hello Coders! :)");
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const skipLink = document.querySelector(".skip-link");
+//   const mainContent = document.querySelector("#main-content");
+
+//   skipLink.addEventListener("focus", function () {
+//     skipLink.style.left = "auto";
+//   });
+
+//   mainContent.addEventListener("focus", function () {
+//     skipLink.style.left = "-999em";
+//   });
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerButton = document.getElementById("hamburger-button");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburgerButton.addEventListener("click", function () {
+    navLinks.classList.toggle("show");
+  });
+});
+
+const restaurantList = document.getElementById("restaurantList");
+const restaurantData = {
+  restaurants: [
+    {
+      id: "6c7bqjgi84kcowlqdz",
+      name: "Bring Your Phone Cafe",
+      description: "Lorem ipsum dolor sit amet...",
+      pictureId: "https://restaurant-api.dicoding.dev/images/medium/41",
+      city: "Medan",
+      rating: 4.6,
+    },
+    {
+      id: "ljx8i0qu2uckcowlqdz",
+      name: "Run The Gun",
+      description:
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.",
+      pictureId: "https://restaurant-api.dicoding.dev/images/medium/07",
+      city: "Bali",
+      rating: 4.6,
+    },
+    {
+      id: "fe8bbxoazddkcowlqdz",
+      name: "Pangsit Express",
+      description:
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.",
+      pictureId: "https://restaurant-api.dicoding.dev/images/medium/29",
+      city: "Ternate",
+      rating: 4.8,
+    },
+    {
+      id: "ik1zljmlf68kcowlqdz",
+      name: "Ducky Duck",
+      description:
+        "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.",
+      pictureId: "https://restaurant-api.dicoding.dev/images/medium/38",
+      city: "Malang",
+      rating: 4.7,
+    },
+    {
+      id: "9jpuzkm6n6jkcowlqdz",
+      name: "Kafein",
+      description:
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.",
+      pictureId: "https://restaurant-api.dicoding.dev/images/medium/40",
+      city: "Bali",
+      rating: 3.8,
+    },
+    {
+      id: "cpl5jpsnuqkkcowlqdz",
+      name: "Makan mudah",
+      description:
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.",
+      pictureId: "https://restaurant-api.dicoding.dev/images/medium/08",
+      city: "Malang",
+      rating: 4.6,
+    },
+    {
+      id: "iqtf9hmdzvbkcowlqdz",
+      name: "Saya Suka",
+      description:
+        "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.",
+      pictureId: "https://restaurant-api.dicoding.dev/images/medium/32",
+      city: "Surabaya",
+      rating: 3.6,
+    },
+    {
+      id: "8i06gqcc2dpkcowlqdz",
+      name: "Gigitan Cepat",
+      description:
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.",
+      pictureId: "https://restaurant-api.dicoding.dev/images/medium/45",
+      city: "Aceh",
+      rating: 4,
+    },
+    {
+      id: "wf5o19xhxxkcowlqdz",
+      name: "Fairy Cafe",
+      description:
+        "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.",
+      pictureId: "https://restaurant-api.dicoding.dev/images/medium/04",
+      city: "Malang",
+      rating: 3.9,
+    },
+  ],
+};
+
+function createRestaurantCard(restaurant) {
+  const card = document.createElement("div");
+  card.classList.add("restaurant-card");
+
+  const image = document.createElement("img");
+  image.src = restaurant.pictureId;
+  image.alt = restaurant.name;
+
+  const name = document.createElement("h2");
+  name.textContent = restaurant.name;
+
+  const city = document.createElement("p");
+  city.textContent = `Kota: ${restaurant.city}`;
+
+  const rating = document.createElement("p");
+  rating.textContent = `Rating: ${restaurant.rating}`;
+
+  const description = document.createElement("p");
+  description.textContent = restaurant.description;
+
+  card.appendChild(image);
+  card.appendChild(name);
+  card.appendChild(city);
+  card.appendChild(rating);
+  card.appendChild(description);
+
+  return card;
+}
+
+restaurantData.restaurants.forEach((restaurant) => {
+  const card = createRestaurantCard(restaurant);
+  restaurantList.appendChild(card);
+});
